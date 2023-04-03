8a61ae7eb7c9d34d3aef482bdee5628414d198a0
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-03 13:51:51 +0300

    adding files

--- src/App.vue
+++ src/App.vue
@@ -17,35 +17,3 @@ export default {
     }
 }
 </script>
-<style>
-*, *::before, *::after {
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-}
-body {
-    padding: 0;
-    margin: 0 !important;
-}
-
-.grid {
-    display: grid;
-    grid-template-columns: 4fr 8fr;
-    grid-gap: 20px;
-}
-
-.main {
-    background-color: #fff;
-
-}
-
-.sidebar {
-    background-color: #1c2e4a !important;
-    color: #fff;
-    display: flex !important;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-}
-</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -13,9 +13,35 @@ export default {
 
 }
 </script>
-<style scoped>
-a {
+<style>
+*, *::before, *::after {
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+body {
+    padding: 0;
+    margin: 0 !important;
+}
+
+.grid {
+    display: grid;
+    grid-template-columns: 4fr 8fr;
+    grid-gap: 20px;
+}
+
+.main {
+    background-color: #fff;
+
+}
+
+.sidebar {
+    background-color: #1c2e4a !important;
     color: #fff;
-    text-decoration: none;
+    display: flex !important;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
 }
 </style>
