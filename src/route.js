247e143588e2c9312af439e84dd1d70d51989c5b
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-03 10:09:07 +0300

    adding files

--- src/route.js
+++ src/route.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from '@/components/Home.vue';
+import Home from './components/Home.vue';
+import Sidebar from './components/Sidebar.vue';
 
 const routes = [
     {
