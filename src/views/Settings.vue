f0933b480b80754b1044785cf78ac67e84e211f1
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-03 13:48:04 +0300

    adding files

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,6 +1,7 @@
 <template>
     <h1>Settings</h1>
     <router-link to="/settings/email" >Email</router-link>
+    <router-view name="subscriptions"></router-view>
 </template>
 <script>
 export default {
