97239e86c5f6e781fd193af23b050d76ba83e772
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-03 13:50:45 +0300

    adding files

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,16 +1,20 @@
 <template>
-    <h1>Settings</h1>
-    <router-link to="/settings/email" >Email</router-link>
-    <router-view name="subscriptions"></router-view>
+    <sidebar>
+        <h1>Settings</h1>
+        <router-link to="/settings/email">Email</router-link>
+    </sidebar>
+    <main>
+        <router-view name="subscriptions"></router-view>
+    </main>
 </template>
 <script>
 export default {
     name: 'Settings',
-    
+
 }
 </script>
 <style scoped>
-a{
+a {
     color: #fff;
     text-decoration: none;
 }
