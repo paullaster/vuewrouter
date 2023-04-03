d70c93ba6b66c4a3e527823ba8aadb9b2e0b5179
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-03 09:28:07 +0300

    adding files

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            message:'Hello from Vue'
+        }
+    }
+}
+</script>
